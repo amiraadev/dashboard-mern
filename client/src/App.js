ab3382599e87fe5811b68e8650a7668b6cd19e34
amirticha <allagui_fawzi@outlook.fr>
2023-07-23 11:16:34 +0100

    rouing commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 
+import { CssBaseline , ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "scenes/Dashboard";
+import Layout from "scenes/Layout";
+import { themeSettings } from "theme";
+
+
 
 function App() {
+  const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)),[mode])
   return (
     <div className="app">
-     
+      <BrowserRouter >
+        <Routes>
+            <Route element={<Layout/>}>            
+                <Route path="/" element={<Navigate to ="/dashboard" replace />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+        </Routes>
+      </BrowserRouter>
+           <ThemeProvider theme={theme} >
+          <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
